cc3a66b972b9b03232f7088be4045cba0f50dbcf
Taisheng Chen <ists.chen@gmail.com>
2024-04-07 22:39:07 +0300

    修改

--- server/login.sql
+++
@@ -1,9 +0,0 @@
-
-create table users (
-  id serial primary key,
-  username varchar(100) unique not null,
-  email varchar(100) unique not null,
-  password varchar(255) not null
-);
-
-insert into users (username,email,password) values ('admin','admin@123','admin123');
